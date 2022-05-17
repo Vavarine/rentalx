1d1513b2ac9ad6aad363d12050765c02143497bb
vavarine <evailson.m.barbosa@gmail.com>
2022-05-17 09:18:19 -0300

    feat: create repository

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import { categoriesRoutes } from './categories.routes';
+import { specificationRoutes } from './specifications.routes';
 
 const routes = Router();
 
@@ -9,5 +10,6 @@ routes.get('/', (_req, res) => {
 });
 
 routes.use('/categories', categoriesRoutes);
+routes.use('/specifications', specificationRoutes);
 
 export { routes };
